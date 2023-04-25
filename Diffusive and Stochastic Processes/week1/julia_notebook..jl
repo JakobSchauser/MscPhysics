a1253fcc8981b4f857a9ff977dfb4c8570ed958e
JakobSchauser <jakob.schauser@gmail.com>
2023-04-25 16:27:58 +0200

    started week1

---
+++ Diffusive and Stochastic Processes/week1/julia_notebook..jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c1e5d8bb-a646-47f8-a7d4-f69071f911ab
+i'm new here!
+
+# ╔═╡ Cell order:
+# ╠═c1e5d8bb-a646-47f8-a7d4-f69071f911ab
---
+++ Diffusive and Stochastic Processes/week1/randomwalk.jl
@@ -0,0 +1,12 @@
+using Plots
+
+y = rand(100)
+
+p = plot(y)
+display(p)
+
+
+#     histogram(movers[1,:], )
+# end
+
+# walk(2,10)
